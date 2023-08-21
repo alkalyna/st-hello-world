59a61eee4e73eebc11527fc8a77e7fd74b3ee20a
Muhammad Rifqi Abdurrahman <136041775+alkalyna@users.noreply.github.com>
2023-08-21 21:35:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,5 @@ st.session_state.disabled = True
 # input nama visitor
 name = st.text_input("Siapa nama kamu? ")
 
-# Say hello to visitor
-output = print("Hello, ", name +" thank you for visiting.")
-
 # output akhir di streamlit
-st.write(output)
+st.write("Hello, ", name," thank you for visiting")
