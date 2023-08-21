a2c1bd6fdde45c232722f7ab7b522b187fcba03e
Muhammad Rifqi Abdurrahman <136041775+alkalyna@users.noreply.github.com>
2023-08-21 21:44:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.session_state.disabled = True
 
 # input nama visitor
-name = st.text_input("Siapa nama kamu? ")
+name = st.text_input("Siapa nama kamu? / What is your name? ")
 
 # output akhir di streamlit
 if name:
