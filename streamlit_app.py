556281c258f7acbcfc1fb4e1e0af273293e9fbc5
Muhammad Rifqi Abdurrahman <136041775+alkalyna@users.noreply.github.com>
2023-08-21 21:31:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("hello world it's me, baby girl Alkalyna")
+
+st.session_state.disabled = True
+
+# input nama visitor
+name = st.text_input("Siapa nama kamu? ")
+
+# Say hello to visitor
+output = print("Hello, ", name +" thank you for visiting.", :smile:)
+
+# output akhir di streamlit
+st.write(output)
